3b327074f4050b0e351b4b8969b51ba7071349a8
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-04-26 18:55:17 -0500

    Tipos de numeros enteros y flotantes

---
+++ src/Flotantes.java
@@ -0,0 +1,15 @@
+public class Flotantes {
+    public static void main (String args []){
+        
+        float numeroFlotante = (float)10.22;
+        System.out.println("numeroFlotante = " + numeroFlotante);
+        System.out.println("Valor minimo tipo float: " + Float.MIN_VALUE);
+        System.out.println("Valor maximo tipo float: " + Float.MAX_VALUE);
+        
+        double numeroDoble = 15.552D;
+        System.out.println("numeroDoble = " + numeroDoble);
+        System.out.println("Valor minimo tipo double: " + Double.MIN_VALUE);
+        System.out.println("Valor maximo tipo double: " + Double.MAX_VALUE);
+        
+    }
+}
