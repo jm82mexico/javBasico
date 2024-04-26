cff6cc4eefdf34a96ecc92076339b4a43b1f23f9
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-04-25 20:37:16 -0600

    Caracteres especiales

---
+++ src/caracteresEspeciales.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jmdel
+ */
+public class caracteresEspeciales {
+    public static void main (String args[]){
+        var nombre = "Luche";
+        
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b\b" + nombre);
+        System.out.println("Comilla simple \'" + nombre + "\'");
+        System.out.println("Comilla doble \"" + nombre + "\"");
+    }
+}
