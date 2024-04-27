f61e1cc8c7b6fb83766bcb1bc1e513ab3e8accd3
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-04-26 19:06:29 -0600

    Prueba Ide

--- src/TipoChar.java
+++ src/TipoChar.java
@@ -2,7 +2,7 @@
 public class TipoChar {
     public static void main(String args[]){
         
-        //Se puede declarar con su codigo unicode
+//        Se puede declarar con su codigo unicode
         char Char = '\u0021';
         System.out.println("Char = " + Char);
         //Con su representación decimal
@@ -12,15 +12,15 @@ public class TipoChar {
         char CharSimbolo = '!';
         System.out.println("CharSimbolo = " + CharSimbolo);       
         
-        //INFERECIA DE TIPOS
-        
-        //Se puede declarar con su codigo unicode
+//        INFERECIA DE TIPOS
+
+//        Se puede declarar con su codigo unicode
         var Char2 = '\u0021';
         System.out.println("Char = " + Char2);
-        //NO LO INFIERE COMO CHAR SINO COMO ENTERO
+//        NO LO INFIERE COMO CHAR SINO COMO ENTERO
         var CharDecimal2 = 33;
         System.out.println("CharDecimal = " + CharDecimal2);
-        //Se puede declarar con su simbolo
+//        Se puede declarar con su simbolo
         var CharSimbolo2 = '!';
         System.out.println("CharSimbolo = " + CharSimbolo2);    
     }
